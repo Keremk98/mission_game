cd9cbba1bfb8dd9a15def2d5e694ed2e1d552038
Keremk98 <77936104+Keremk98@users.noreply.github.com>
2021-03-16 22:12:27 -0500

    Add files via upload

---
+++ Chapter 3 Milestone 3.py
@@ -0,0 +1,12 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Chapter 3
+>>> userInput = input("Press 'x' to break the window of cab/bus").capitalize()
+Press 'x' to break the window of cab/bus x
+>>> window_broken = 'x'
+>>> if window_broken:
+	print("Mission completed, move to Chapter 4")
+
+	
+Mission completed, move to Chapter 4
+>>> 
